d5a22a76730824060123c64cdaab72dcf6078dbe
monirajan <a.monisha.1995@gmail.com>
2020-12-08 23:27:24 +0530

    Commit on DI with profile and qualifiers

--- src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
@@ -1,20 +1,19 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class ConstructorInjectedController {
+public class I18nController {
 
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    public String getGreeting()
+    public String sayHello()
     {
         return greetingService.sayGreeting();
     }
--- src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyInjectedControllerTest {
 
@@ -12,7 +10,7 @@ class PropertyInjectedControllerTest {
     @BeforeEach
     void setUp() {
         propertyInjectedController = new PropertyInjectedController();
-        propertyInjectedController.greetingService = new GreetingServiceImpl();
+        propertyInjectedController.greetingService = new ConstructorGreetingService();
     }
 
     @Test
